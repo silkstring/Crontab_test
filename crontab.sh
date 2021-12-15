713e4e8b030947198bb73ef92548427496dc6f71
silkstring <why612@naver.com>
2021-12-15 20:26:52 +0900

    Wed Dec 15 20:26:52 KST 2021 commit test

--- crontab.sh
+++ crontab.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-#!/bin/sh
-DATE=$(date)
-echo $DATE >> /root/silk/Crontab_test/test.txt
+cd /root/silk/Crontab_test
+git add .
+git checkout -b master
+echo $(date) >> test.txt
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/silk
-git commit -am "$DATE commit!"
-git push -u
 
-echo "git clear"
+git commit -am "$(date) commit test"
+
+git push --set-upstream origin master
