1acda96b6ce15e1aeaed5d6faff1c8f384d29390
silkstring <why612@naver.com>
2021-12-15 20:48:39 +0900

    Wed Dec 15 20:48:39 KST 2021 commit test

--- crontab.sh
+++ crontab.sh
@@ -2,15 +2,15 @@
 cd /root/silk/Crontab_test
 git add .
 git checkout -b master
-echo $(date) >> test.txt
+echo $(date) >> /root/silk/Crontab_test/test.txt
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/silk
 
-git commit -am "$(date) commit test" >> result.txt
+git commit -am "$(date) commit test" >> /root/silk/Crontab_test/result.txt
 
-git push --set-upstream origin master >> result.txt
+git push --set-upstream origin master >> /root/silk/Crontab_test/result.txt
 
-cat result.txt | mail -s "[git plan] crontab auto" dldudgjs31@naver.com
+cat /root/silk/Crontab_test/result.txt | mail -s "[git plan] crontab auto" dldudgjs31@naver.com
 
 cat /dev/null > result.txt
 
