7534bcd6d8c53b15abbfe178506646d9bc110d1e
silkstring <why612@naver.com>
2022-01-05 12:00:03 +0900

    Wed Jan  5 12:00:02 KST 2022 commit test

--- random.sh
+++ random.sh
@@ -2,9 +2,13 @@
 
 random_num="$(($RANDOM% 10))"
 echo "$(date) num: $random_num" >> /root/silk/Crontab_test/today_num.log
+
+if [ "$random_num" == "0" ];then
+	 sh /root/silk/Crontab_test/crontab.sh
+fi
+	
 for (( n=1; n<=$random_num; n++))
 do
-#	echo "hi"
 	sh /root/silk/Crontab_test/crontab.sh
 done
 
