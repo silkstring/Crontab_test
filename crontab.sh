2f30a26ea6e41f2ac561a75398cdcae58ae0801c
silkstring <why612@naver.com>
2022-01-13 14:07:56 +0900

    Thu Jan 13 14:07:56 JST 2022 commit test

--- crontab.sh
+++ crontab.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
-cd /root/silk/Crontab_test/
+cd /home/youngtec/git/Crontab_test
 
-/root/bin/git config --global user.name silkstring
-/root/bin/git config --global user.mail why612@naver.com
-/root/bin/git config --global user.email why612@naver.com
-/root/bin/git config --list > /root/silk/Crontab_test/git_id_check
+/usr/local/cpanel/3rdparty/lib/path-bin/git config --global user.name silkstring
+/usr/local/cpanel/3rdparty/lib/path-bin/git config --global user.mail why612@naver.com
+/usr/local/cpanel/3rdparty/lib/path-bin/git config --global user.email why612@naver.com
+/usr/local/cpanel/3rdparty/lib/path-bin/git config --list > /root/silk/Crontab_test/git_id_check
 
-cd /root/silk/Crontab_test/
+cd /home/youngtec/git/Crontab_test
 
-/root/bin/git add .
+/usr/local/cpanel/3rdparty/lib/path-bin/git add .
 
-/root/bin/git checkout -b main
+/usr/local/cpanel/3rdparty/lib/path-bin/git checkout -b main
 
-if [ "$(cat /root/silk/Crontab_test/test.txt | wc -l)" -gt 30 ]; then
-	sed -i '1,30d' /root/silk/Crontab_test/test.txt
+if [ "$(cat /home/youngtec/git/Crontab_test/test.txt | wc -l)" -gt 30 ]; then
+	sed -i '1,30d' /home/youngtec/git/Crontab_test/test.txt
 fi
 
 echo $(date) >> test.txt
 
-/root/bin/git add .
+/usr/local/cpanel/3rdparty/lib/path-bin/git add .
 
 eval $(ssh-agent -s)
 
 ssh-add ~/.ssh/silk
 
-/root/bin/git commit -am "$(date) commit test" 
+/usr/local/cpanel/3rdparty/lib/path-bin/git commit -am "$(date) commit test" 
 
-/root/bin/git push --set-upstream origin main
+/usr/local/cpanel/3rdparty/lib/path-bin/git push --set-upstream origin main
 
-sh /root/silk/Crontab_git_push/crontab.sh
+#sh /root/silk/Crontab_git_push/crontab.sh
